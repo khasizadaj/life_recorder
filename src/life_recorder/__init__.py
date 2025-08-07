3bcf9b46393dd7326d4986769f1631b8681bd771
khasizadaj <javid.khasizada@gmail.com>
2025-08-07 21:08:19 +0200

    Update structure

---
+++ src/life_recorder/__init__.py
@@ -0,0 +1,5 @@
+"""Life Recorder - A CLI tool for recording life events."""
+
+__version__ = "0.1.0"
+__author__ = "Javid Khasizada"
+__email__ = "willow.tiger8627@eagereverest.com"
---
+++ src/life_recorder/cli.py
@@ -0,0 +1,71 @@
+"""Module is used to run LifeRecorder class and record some memories."""
+
+import sys
+from typing import List, Tuple
+from loguru import logger
+
+import life_recorder.check as ch
+import life_recorder.factory.factory as fac
+from life_recorder.factory.base import COMMANDS
+
+
+def get_arguments(args: List[str]) -> Tuple[bool]:
+    """Function checks whether arguments are valid."""
+
+    command = args[0]
+    is_command = ch.check_command(command)
+
+    if not is_command:
+        sys.exit()
+
+    # `None` is defualt value for variable, which means that identifier is not
+    # required for this command. if it's required but not provided or provided
+    # incorrectly, respective message will be printed.
+    identifier = None
+
+    requires_identifier: bool = ch.check_requires_identifier(command)
+    if requires_identifier:
+        identifier_status = ch.get_identifier_status(args)
+        status_function = ch.STATUS_FUNCS.get(identifier_status, None)
+        if status_function is not None:
+
+            # status of 1 means that the identifier is not provided for the
+            # command. As `read` can perform action with or without the identifier,
+            # this check omots that specific type of error.
+            if identifier_status == 1 and command in COMMANDS.read:
+                pass
+            else:
+                status_function(command)
+                sys.exit()
+        else:
+            identifier = args[1]
+
+    return command, identifier
+
+
+@logger.catch
+def main() -> None:
+    """Functions gets the record and writes it to the file."""
+
+    arguments = sys.argv[1:]
+    if len(arguments) < 1:
+        print('Error: You haven\'t provided any arguments for program to perform '
+              'action.')
+        print('Usage: Add commands to use this program. Supported commands: '
+              'create, read, read <identifier>, update <identifier>, '
+              'delete <identifier>')
+
+        sys.exit()
+
+    command, identifier = get_arguments(arguments)
+
+    # get factory
+    action = fac.action_factory(command)
+
+    # perform the action
+    action().act(identifier)
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
