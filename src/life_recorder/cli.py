54233770e467990ad81ea911c0e3dbbc1fdeab30
khasizadaj <javid.khasizada@gmail.com>
2025-08-08 18:16:07 +0200

    Add support for Click for CLI

--- src/life_recorder/cli.py
+++ src/life_recorder/cli.py
@@ -3,69 +3,31 @@
 import sys
 from typing import List, Tuple
 from loguru import logger
+import click
 
 import life_recorder.check as ch
 import life_recorder.factory.factory as fac
-from life_recorder.factory.base import COMMANDS
 
 
-def get_arguments(args: List[str]) -> Tuple[bool]:
-    """Function checks whether arguments are valid."""
-
-    command = args[0]
-    is_command = ch.check_command(command)
-
-    if not is_command:
-        sys.exit()
-
-    # `None` is defualt value for variable, which means that identifier is not
-    # required for this command. if it's required but not provided or provided
-    # incorrectly, respective message will be printed.
-    identifier = None
-
-    requires_identifier: bool = ch.check_requires_identifier(command)
-    if requires_identifier:
-        identifier_status = ch.get_identifier_status(args)
-        status_function = ch.STATUS_FUNCS.get(identifier_status, None)
-        if status_function is not None:
-
-            # status of 1 means that the identifier is not provided for the
-            # command. As `read` can perform action with or without the identifier,
-            # this check omots that specific type of error.
-            if identifier_status == 1 and command in COMMANDS.read:
-                pass
-            else:
-                status_function(command)
-                sys.exit()
-        else:
-            identifier = args[1]
-
-    return command, identifier
-
-
+@click.command("main")
+@click.argument(
+    "command", type=click.Choice(["create", "read", "update", "delete"])
+)
+@click.argument("identifier", required=False, type=click.STRING)
+@click.version_option(version="0.1.0", prog_name="life_recorder")
 @logger.catch
-def main() -> None:
-    """Functions gets the record and writes it to the file."""
+def main(command: str, identifier: str) -> None:
+    """
+    Utility to record life events.
+    """
 
-    arguments = sys.argv[1:]
-    if len(arguments) < 1:
-        print('Error: You haven\'t provided any arguments for program to perform '
-              'action.')
-        print('Usage: Add commands to use this program. Supported commands: '
-              'create, read, read <identifier>, update <identifier>, '
-              'delete <identifier>')
+    click.echo(f"Command: {command}")
+    click.echo(f"Identifier: {identifier}")
 
-        sys.exit()
-
-    command, identifier = get_arguments(arguments)
-
-    # get factory
     action = fac.action_factory(command)
-
-    # perform the action
-    action().act(identifier)
+    action.act(identifier)
     sys.exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
