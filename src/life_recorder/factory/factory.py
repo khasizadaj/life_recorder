b560ff7d3b94516ec66d29a95b97dfdb7dd4d450
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2025-08-09 16:21:04 +0200

    Clean `Commands` class (#17)

--- src/life_recorder/factory/factory.py
+++ src/life_recorder/factory/factory.py
@@ -1,7 +1,7 @@
 """Module contains creation part of factory pattern."""
 
 # factories
-from .base import LifeRecorder, COMMANDS
+from .base import LifeRecorder, Commands
 from .create import CreateLifeRecorder
 from .read import ReadLifeRecorder
 from .update import UpdateLifeRecorder
@@ -11,13 +11,13 @@ from .delete import DeleteLifeRecorder
 def action_factory(command: str) -> LifeRecorder:
     """Factory that decides which action to perform based on the command."""
 
-    if command in COMMANDS.create:
+    if command == Commands.CREATE:
         return CreateLifeRecorder()
-    elif command in COMMANDS.read:
+    elif command == Commands.READ:
         return ReadLifeRecorder()
-    elif command in COMMANDS.update:
+    elif command == Commands.UPDATE:
         return UpdateLifeRecorder()
-    elif command in COMMANDS.delete:
+    elif command == Commands.DELETE:
         return DeleteLifeRecorder()
     else:
         raise ValueError("Command doesn't exist.")
