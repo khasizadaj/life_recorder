c2449a2bb93f27a34fa1a0830a21a4e16f56b592
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2025-08-13 12:58:47 +0200

    Enhance add delete to UI (#24)

--- src/life_recorder/app.py
+++ src/life_recorder/app.py
@@ -8,6 +8,7 @@ from textual.widgets import (
     ListView,
     Markdown,
     Rule,
+    Button,
 )
 from textual import log
 
@@ -16,7 +17,40 @@ from life_recorder.base import LifeRecorder
 DB = LifeRecorder()
 
 
+class ViewingPane(VerticalScroll):
+    """A pane to view the details of a selected note."""
+
+    def __init__(self, *args, **kwargs):
+        self.record_id = kwargs.pop("record_id", "default")
+        super().__init__(*args, **kwargs)
+
+    def compose(self) -> ComposeResult:
+        yield Markdown("# Note View", id="note-title")
+        yield Rule()
+        yield Label("🏷️ new-note", expand=True, id="note-tag")
+        yield Label("⏳ soon...", expand=True, id="note-timestamp")
+        yield Rule()
+        yield Markdown(
+            "\n\nNote details will be displayed here.", id="note-content"
+        )
+        yield Button("Delete", variant="error", id="button-delete")
+
+    def reset(self):
+        """Reset the viewing pane to its default state."""
+        self.record_id = "default"
+        self.query_one("#note-title", Markdown).update("# Note View")
+        self.query_one("#note-tag", Label).update("🏷️ new-note")
+        self.query_one("#note-timestamp", Label).update("⏳ soon...")
+        self.query_one("#note-content", Markdown).update(
+            "\n\nNote details will be displayed here."
+        )
+        for button in self.query(Button):
+            button.styles.display = "none"
+
+
 class Notes(HorizontalScroll):
+    BINDINGS = [("escape", "reset_view", "Reset the viewing pane")]
+
     def compose(self) -> ComposeResult:
         yield ListView(
             *[
@@ -27,42 +61,18 @@ class Notes(HorizontalScroll):
             ],
             id="list-view",
         )
-        yield VerticalScroll(
-            Markdown("# Note View", id="note-title"),
-            Rule(),
-            Label(" 🏷️ new-note", expand=True, id="note-tag"),
-            Label(" ⏳ soon...", expand=True, id="note-timestamp"),
-            Rule(),
-            Markdown(
-                "\n\nNote details will be displayed here.", id="note-content"
-            ),
+        yield ViewingPane(
+            record_id="default",
             id="note-view",
+            can_focus=False,
+            can_focus_children=True,
         )
 
-
-class LifeRecorderApp(App):
-    """A Textual app to manage life records"""
-
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
-
-    def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
-        yield Header(show_clock=True, name="Life Recorder")
-        yield VerticalScroll(
-            Markdown(
-                "## Welcome to Life Recorder. \nReady to jot down your thoughts?"
-            ),
-            Notes(),
-        )
-        yield Footer()
-
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.theme = (
-            "textual-dark"
-            if self.theme == "textual-light"
-            else "textual-light"
-        )
+    def action_reset_view(self) -> None:
+        self.query_one("#note-view", ViewingPane).reset()
+        log.info("Escape key pressed, viewing pane reset to default state.")
+        list_view = self.query_one("#list-view", ListView)
+        list_view.post_message(message=ListView.Highlighted(list_view, None))
 
     async def on_list_view_selected(self, event: ListView.Selected) -> None:
         list_view = event.control
@@ -76,6 +86,9 @@ class LifeRecorderApp(App):
         ):
             raise ValueError("Record details do not match expected structure.")
 
+        viewing_pane = self.query_one("#note-view", ViewingPane)
+        viewing_pane.record_id = details.get("id", "default")
+
         title = self.query_one("#note-title", Markdown)
         if title.source != details.get("title", ""):
             title.update(
@@ -84,15 +97,71 @@ class LifeRecorderApp(App):
 
         tag = self.query_one("#note-tag", Label)
         if tag.renderable != details.get("tag", ""):
-            tag.update(f" 🏷️ {details.get('tag', '')}")
+            tag.update(f"🏷️ {details.get('tag', '')}")
 
         timestamp = self.query_one("#note-timestamp", Label)
         if timestamp.renderable != details.get("timestamp", ""):
-            timestamp.update(f" ⏳ {details.get('timestamp', '')}")
+            timestamp.update(f"⏳ {details.get('timestamp', '')}")
 
         content = self.query_one("#note-content", Markdown)
         if content.source != details.get("content", ""):
             content.update(details.get("content", ""))
+
+        buttons = viewing_pane.query(Button)
+        for button in buttons:
+            button.styles.display = "block"
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.control.id
+        if button_id is None:
+            return
+
+        if button_id == "button-delete":
+            log.info("Delete button pressed.")
+            viewing_pane = self.query_one("#note-view", ViewingPane)
+
+            log.info(f"Deleting note with ID of {viewing_pane.record_id}")
+            try:
+                DB.delete(viewing_pane.record_id)
+            except ValueError as e:
+                log.error(f"Invalid record ID: {e}")
+                return
+
+            list_item = self.query_one(f"#{viewing_pane.record_id}", ListItem)
+            list_item.remove()
+
+            log.info(f"Deleted record with ID: {viewing_pane.record_id}")
+            viewing_pane.reset()
+            log.info("Viewing pane reset to default state.")
+
+class LifeRecorderApp(App):
+    """A Textual app to manage life records"""
+
+    CSS_PATH = "styles/app.tcss"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header(show_clock=True, name="Life Recorder")
+        yield VerticalScroll(
+            Markdown(
+                "## Welcome to Life Recorder. \nReady to jot down your thoughts?"
+            ),
+            Notes(can_focus=False, can_focus_children=True, id="notes"),
+            can_focus=False,
+            can_focus_children=True,
+        )
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark"
+            if self.theme == "textual-light"
+            else "textual-light"
+        )
+
+
 
 
 def main():
