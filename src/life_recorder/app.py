c482332e95e0de4f71f769c6da1b848abc809c4d
khasizadaj <javid.khasizada@gmail.com>
2025-08-15 20:16:51 +0200

    Add note form to be used later

--- src/life_recorder/app.py
+++ src/life_recorder/app.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.containers import HorizontalScroll, VerticalScroll
+from textual.containers import HorizontalScroll, VerticalScroll, VerticalGroup
 from textual.widgets import (
     Footer,
     Header,
@@ -9,12 +9,124 @@ from textual.widgets import (
     Markdown,
     Rule,
     Button,
+    Input,
+    Static,
 )
-from textual import log
+from textual.validation import Length
+from textual import log, on
 
 from life_recorder.base import LifeRecorder
 
 DB = LifeRecorder()
+
+
+class NewNoteForm(Static):
+
+    BINDINGS = [
+        ("ctrl+enter", "action_submit", "Submit the form"),
+        ("enter", "action_submit", "Submit the form"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        with VerticalGroup(id="new-note-container", classes="new-note", disabled=True):
+            yield Markdown("### Add new note")
+            yield Static("")
+
+            yield Label("Title")
+            yield Input(
+                placeholder="Title",
+                id="new-note-title",
+                validate_on=["submitted"],
+                validators=[Length(minimum=1, maximum=50)],
+            )
+            yield Label("", variant="error", classes="error", id="new-note-error-title")
+            yield Static("")
+
+            yield Label("Content")
+            yield Input(
+                placeholder="Content",
+                validate_on=["submitted"],
+                validators=[Length(minimum=1, maximum=50)],
+                id="new-note-content",
+            )
+            yield Label("", variant="error", classes="error", id="new-note-error-content")
+            yield Static("")
+
+            yield Label("Tag (Optional)")
+            yield Input(
+                placeholder="Tag",
+                valid_empty=True,
+                id="new-note-tag",
+                validate_on=["submitted"],
+                validators=[Length(minimum=2, maximum=25)],
+            )
+            yield Label("Tag is invalid.", variant="error", classes="error", id="new-note-error-tag")
+            yield Static("")
+
+            yield Button("Save", variant="primary", id="button-save")
+
+    # async def on_button_pressed(self, event: Input.Submitted) -> None:
+    #     """Handle input submission."""
+    #     await self.validate_form()
+    #     await self.create_note()
+
+    @on(Input.Submitted)
+    @on(Button.Pressed, selector="#button-save")
+    async def submit(self, _: Button.Pressed | Input.Submitted) -> None:
+        await self.validate_form()
+        await self.create_note()
+
+    async def validate_form(self):
+        tag_input = self.tag_input
+        res = tag_input.validate(tag_input.value)
+        log.info(f"Tag input validation result: {res}")
+        tag_error_label = self.query_one("#new-note-error-tag", Label)
+        if res is not None and not res.is_valid:
+            description = res.failures[0].description
+            if description is not None:
+                tag_error_label.update(description)
+                log.error(f"Tag input validation failed: {description}")
+            tag_error_label.styles.display = "block"
+            return
+        else:
+            tag_error_label.styles.display = "none"
+
+        title_input = self.title_input
+        res = title_input.validate(title_input.value)
+        log.info(f"Title input validation result: {res}")
+        title_error_label = self.query_one("#new-note-error-title", Label)
+        if res is not None and not res.is_valid:
+            description = res.failures[0].description
+            if description is not None:
+                title_error_label.update(description)
+                log.error(f"Title input validation failed: {description}")
+            title_error_label.styles.display = "block"
+            return
+        else:
+            title_error_label.styles.display = "none"
+
+        content_input = self.content_input
+        res = content_input.validate(content_input.value)
+        log.info(f"Content input validation result: {res}")
+
+    async def create_note(self) -> None:
+
+        log.info("Creating new note with provided details.")
+        log.info(f"Tag: {self.tag_input.value}")
+        log.info(f"Title: {self.title_input.value}")
+        log.info(f"Content: {self.content_input.value}")
+
+    @property
+    def tag_input(self):
+        return self.query_one("#new-note-tag", Input)
+
+    @property
+    def title_input(self):
+        return self.query_one("#new-note-title", Input)
+
+    @property
+    def content_input(self):
+        return self.query_one("#new-note-content", Input)
 
 
 class ViewingPane(VerticalScroll):
@@ -25,6 +137,8 @@ class ViewingPane(VerticalScroll):
         super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
+        # TODO Activate form
+        # yield NewNoteForm()
         yield Markdown("# Note View", id="note-title")
         yield Rule()
         yield Label("🏷️ new-note", expand=True, id="note-tag")
@@ -60,9 +174,7 @@ class Notes(HorizontalScroll):
             )
             yield ListView(
                 *[
-                    ListItem(
-                        Label(f"[ #{x['id']} ] {x['title']}"), id=x["id"]
-                    )
+                    ListItem(Label(f"[ #{x['id']} ] {x['title']}"), id=x["id"])
                     for x in DB.records.values()
                 ],
                 id="list-view",
