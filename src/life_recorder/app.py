ee1f90aaaed30a27b075642f7dfa9a2641055626
khasizadaj <javid.khasizada@gmail.com>
2025-08-20 15:35:48 +0200

    Update note in UI based on input

--- src/life_recorder/app.py
+++ src/life_recorder/app.py
@@ -26,11 +26,31 @@ from life_recorder.base import LifeRecorder
 DB = LifeRecorder()
 
 
-class NewNoteForm(Static):
+class NoteForm(Static):
     BINDINGS = [
         ("ctrl+enter", "action_submit", "Submit the form"),
         ("enter", "action_submit", "Submit the form"),
     ]
+
+    def __init__(
+        self,
+        variant: str | None = None,
+        record_id: str | None = None,
+        *args,
+        **kwargs,
+    ):
+        if variant is not None and variant not in ["new", "update"]:
+            raise ValueError(f"Invalid variant: {self.variant}")
+
+        self.variant = variant if variant else "new"
+
+        if self.variant == "update":
+            if record_id is None:
+                raise ValueError(
+                    "record_id must be provided for update variant"
+                )
+        self.record_id = record_id
+        super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
         with VerticalGroup(id="new-note-container", classes="new-note"):
@@ -86,14 +106,25 @@ class NewNoteForm(Static):
             self.record = record
             super().__init__()
 
+    class Updated(Message):
+        """Note updated message."""
+
+        def __init__(self, record: dict[str, str]) -> None:
+            self.record = record
+            super().__init__()
+
     @on(Input.Submitted)
     @on(Button.Pressed, selector="#button-save")
     async def submit(self, _: Button.Pressed | Input.Submitted) -> None:
         validation_result = await self.validate_form()
         if not validation_result:
             return
-        note = await self.create_note()
-        self.post_message(self.Created(note))
+        if self.variant == "update":
+            note = await self.update_note()
+            self.post_message(self.Updated(note))
+        else:
+            note = await self.create_note()
+            self.post_message(self.Created(note))
         await self.reset()
 
     async def validate_form(self):
@@ -152,6 +183,25 @@ class NewNoteForm(Static):
                 "title": self.title_input.value,
                 "content": self.content_input.value,
             }
+        )
+        return note
+
+    async def update_note(self) -> dict[str, str]:
+        if self.record_id is None:
+            raise ValueError("record_id must be set for update operation")
+
+        log.info(f"Updating note with provided details for {self.record_id}")
+        log.info(f"Tag: {self.tag_input.value}")
+        log.info(f"Title: {self.title_input.value}")
+        log.info(f"Content: {self.content_input.value}")
+
+        note = DB.update(
+            identifier=self.record_id,
+            record={
+                "tag": self.tag_input.value,
+                "title": self.title_input.value,
+                "content": self.content_input.value,
+            },
         )
         return note
 
@@ -239,7 +289,7 @@ class Notes(HorizontalScroll):
     def compose(self) -> ComposeResult:
         with VerticalScroll(can_focus=False, can_focus_children=True):
             yield Button("New Note", id="button-new", variant="primary")
-            yield NewNoteForm()
+            yield NoteForm()
             yield ListView(
                 *[self.create_list_item(x) for x in DB.records.values()],
                 id="list-view",
@@ -253,14 +303,15 @@ class Notes(HorizontalScroll):
         )
 
     @on(Button.Pressed, "#button-new")
-    async def create_new_note(self, event: Button.Pressed) -> None:
+    async def create_new_note(self) -> None:
         log.info("New note button pressed.")
-        new_note_form = self.query_one(NewNoteForm)
+        new_note_form = self.query_one(NoteForm)
         new_note_form.styles.display = "block"
+        new_note_form.variant = "new"
         new_note_form.query_one("#new-note-title", Input).focus()
 
-    @on(NewNoteForm.Created)
-    async def update_list_view(self, event: NewNoteForm.Created) -> None:
+    @on(NoteForm.Created)
+    async def update_list_view(self, event: NoteForm.Created) -> None:
         log.debug(
             f"New note created, updating list view for {event.record['id']}"
         )
@@ -269,7 +320,29 @@ class Notes(HorizontalScroll):
         self.list_view.mount(list_item)
         log.debug("List view updated with new note")
 
-        new_note_form = self.query_one(NewNoteForm)
+        new_note_form = self.query_one(NoteForm)
+        new_note_form.styles.display = "none"
+        log.debug("New note form hidden after submission.")
+
+    @on(NoteForm.Updated)
+    def update_note_content(self, event: NoteForm.Updated) -> None:
+        log.debug(f"Updating note content for {event.record['id']}")
+        viewing_pane = self.query_one("#note-view", ViewingPane)
+        viewing_pane.query_one("#note-tag", Label).update(
+            f"🏷️ {event.record['tag']}"
+        )
+        viewing_pane.query_one("#note-title", Markdown).update(
+            f"# {event.record['title']} [ #{event.record['id']} ]"
+        )
+        viewing_pane.query_one("#note-content", Markdown).update(
+            event.record["content"]
+        )
+
+        self.update_list_view_item(event.record)
+
+        log.debug("Note content updated successfully.")
+
+        new_note_form = self.query_one(NoteForm)
         new_note_form.styles.display = "none"
         log.debug("New note form hidden after submission.")
 
@@ -341,8 +414,11 @@ class Notes(HorizontalScroll):
     @on(Button.Pressed, "#button-update")
     async def update_note(self) -> None:
         log.info("Update note button pressed.")
-        new_note_form = self.query_one(NewNoteForm)
+        new_note_form = self.query_one(NoteForm)
         new_note_form.styles.display = "block"
+        new_note_form.variant = "update"
+        new_note_form.record_id = self.viewing_pane.record_id
+        log.info(f"Preparing to update note with ID: {new_note_form.__dict__}")
 
         list_view = self.list_view
         if not list_view.highlighted_child:
@@ -364,9 +440,19 @@ class Notes(HorizontalScroll):
         list_item = ListItem(label, id=record["id"])
         return list_item
 
+    def update_list_view_item(self, record: dict[str, str]) -> None:
+        list_item = self.query_one(f"#{record['id']}", ListItem)
+        label = list_item.query_one(Label)
+        if list_item:
+            label.update(f"[ #{record['id']} ] {record['title']}")
+
     @property
     def list_view(self) -> ListView:
         return self.query_one("#list-view", ListView)
+
+    @property
+    def viewing_pane(self):
+        return self.query_one("#note-view", ViewingPane)
 
 
 class LifeRecorderApp(App):
